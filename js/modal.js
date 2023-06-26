023a8eab5e82e6a060b6738139e8523afda255f3
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-06-26 11:32:08 +0300

    add no-scroll

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,3 @@
-// (() => {
 const refs = {
   openModalBtn: document.querySelector("[data-modal-open]"),
   openModalBtnEnd: document.querySelector("[data-modal-open-end]"),
@@ -6,11 +5,32 @@ const refs = {
   modal: document.querySelector("[data-modal]"),
 };
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.openModalBtnEnd.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+const onModalOpenBtnClick = (e) => {
+  refs.modal.classList.remove("is-hidden");
+  document.addEventListener("keydown", onEscKeyBtnPress);
+  document.body.classList.add("no-scroll");
+};
 
-function toggleModal() {
-  refs.modal.classList.toggle("is-hidden");
-}
-// })();
+const closeModal = () => {
+  refs.modal.classList.add("is-hidden");
+  document.removeEventListener("keydown", onEscKeyBtnPress);
+  document.body.classList.remove("no-scroll");
+};
+const onEscKeyBtnPress = (e) => {
+  if (e.code === "Escape") {
+    closeModal();
+  }
+};
+
+const onBackdropElClick = (e) => {
+  const { target, currentTarget } = e;
+  if (target !== currentTarget) {
+    return;
+  }
+  closeModal();
+};
+
+refs.openModalBtn.addEventListener("click", onModalOpenBtnClick);
+refs.openModalBtnEnd.addEventListener("click", onModalOpenBtnClick);
+refs.closeModalBtn.addEventListener("click", closeModal);
+refs.modal.addEventListener("click", onBackdropElClick);
